2b5d7cfc95139e655959fb33d4c9c0dd3f1a8882
Tiago Oliveira Silva <tiago.osilva>
2017-03-31 13:46:40 -0300

    acesso ao iframe para captura de texto, feature 2

--- cucumber/features/step_definitions/2_iniciar_processo.rb
+++ cucumber/features/step_definitions/2_iniciar_processo.rb
@@ -12,11 +12,15 @@ Entao(/^clico em Iniciar Processo$/) do
   sleep 1
 
   # aqui entra no frame desejado, dentro do frame que está o elemento
+  # page.driver.browser.switch_to.frame 1
   page.driver.browser.switch_to.frame 1
   click_button 'Iniciar Processo'
-
+  sleep 1
+  #entra no iframe e busca o número do protocolo
+  within_frame('_bpmEditor_0_iframe') do
+    protocolo = find('div').find('font').text
+    puts protocolo
+  end
   #Essa linha faz sair do frame acima
   #page.driver.browser.switch_to.default_content
-
-  sleep 10
 end
